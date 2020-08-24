cf03bdb91dd8ba7ca034be154fc3e4c20a32c9c5
Kyungbok Lee <dlrudqhr7180@gmail.com>
2020-08-25 00:10:23 +0900

    test

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 var ps = new kakao.maps.services.Places(map); 
 
 // 카테고리로 은행을 검색합니다
-ps.categorySearch('FD6', placesSearchCB, {useMapBounds:true}); 
+ps.categorySearch('FD6', placesSearchCB, 
+{useMapCenter:true}); 
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB (data, status, pagination) {
