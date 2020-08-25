3a1aa23075427eafa89e6c37ddc4400ac3b5b200
Kyungbok Lee <dlrudqhr7180@gmail.com>
2020-08-25 14:35:35 +0900

    update marker

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니
 
 // 유저 마커 이미지 생성 
 var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', 
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; 
+    imageSize = new kakao.maps.Size(24, 35); 
 
-var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
 
 
