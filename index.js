df075bdf35dc8180bac4cb1beacefe4037d538f8
Kyungbok Lee <dlrudqhr7180@gmail.com>
2020-08-25 14:01:33 +0900

    test

--- index.js
+++ index.js
@@ -6,30 +6,34 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-if (navigator.geolocation) {
-    
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function(position) {
-        
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
-        
-        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-        
-        // 마커와 인포윈도우를 표시합니다
+function findUserPosition() {
+    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+    if (navigator.geolocation) {
+
+        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            var lat = position.coords.latitude, // 위도
+                lon = position.coords.longitude; // 경도
+
+            var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+                message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+
+            // 마커와 인포윈도우를 표시합니다
+            displayCurrentPosition(locPosition, message);
+            catSearch()
+        });
+
+    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+        var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+            message = 'geolocation을 사용할수 없어요..'
+
         displayCurrentPosition(locPosition, message);
-            
-      });
-    
-} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-    
-    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
-        message = 'geolocation을 사용할수 없어요..'
-        
-        displayCurrentPosition(locPosition, message);
+    }
 }
+
+
 
 // 지도에 마커와 인포윈도우를 표시하는 함수입니다
 function displayCurrentPosition(locPosition, message) {
@@ -72,23 +76,26 @@ var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 // 장소 검색 객체를 생성합니다
 var ps = new kakao.maps.services.Places(map); 
 
-// 카테고리로 은행을 검색합니다
-ps.categorySearch('FD6', placesSearchCB, 
-{useMapCenter:true}); 
-
-// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-function placesSearchCB (data, status, pagination) {
-    if (status === kakao.maps.services.Status.OK) {
-        for (var i=0; i<data.length; i++) {
-            displayMarker(data[i]);    
-        }       
+function catSearch(){
+    ps.categorySearch('FD6', placesSearchCB, 
+    {useMapCenter:true}); 
+    
+    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+    function placesSearchCB (data, status, pagination) {
+        if (status === kakao.maps.services.Status.OK) {
+            for (var i=0; i<data.length; i++) {
+                displayMarker(data[i]);    
+            }       
+        }
     }
 }
+
+
 
 // 지도에 마커를 표시하는 함수입니다
 function displayMarker(place) {
     // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
+    var marker = new kakao.maps.Marker({ 
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x) 
     });
@@ -100,3 +107,7 @@ function displayMarker(place) {
         infowindow.open(map, marker);
     });
 }
+
+
+findUserPosition()
+
